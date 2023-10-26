85c300c3c8d36eba8245ff30cec4b2c15e3e92c2
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2023-10-27 02:50:19 +0800

    render card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Filter from './components/Filter/Filter';
+import PropertyCard from './components/PropertyCard/PropertyCard';
 import backendAPI from "./api/backendapi";
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Container , Row, Col} from 'react-bootstrap'  
 
 function App() {
   const [ query, setQuery ] = useState('') 
   const [ data, setData ] = useState([])
+
   useEffect(() => {
     backendAPI.get(`/properties${query}`)
     .then((response) => {
@@ -18,6 +22,7 @@ function App() {
 
   const handleApiCallSuccess = (response) => {
     console.log('GET status', response.status);
+    console.log('GET data', response.data)
     setData(response.data)
   }
 
@@ -32,7 +37,42 @@ function App() {
     <div className="App">
       <h1>Rental App</h1>
       <Filter onSubmitHandler={handleFilterSubmit}/>
-
+      <Container className='p-4'>  
+        <Row>
+          <Col md="4">
+            <PropertyCard 
+              img='https://urhouse.s3.amazonaws.com/images/rentals/567c4bae0aa0cc886831ee6e8aff6646-watermarked.jpg'
+              title='Test'
+              rent='10000'
+              fullAddress='Some Address'
+              closestMrt='Dongmen' />
+          </Col>
+          <Col md="4">
+            <PropertyCard 
+              img='https://urhouse.s3.amazonaws.com/images/rentals/567c4bae0aa0cc886831ee6e8aff6646-watermarked.jpg'
+              title='Test'
+              rent='10000'
+              fullAddress='Some Address'
+              closestMrt='Dongmen' />
+          </Col>
+          <Col md="4">
+            <PropertyCard 
+              img='https://urhouse.s3.amazonaws.com/images/rentals/567c4bae0aa0cc886831ee6e8aff6646-watermarked.jpg'
+              title='Test'
+              rent='10000'
+              fullAddress='Some Address'
+              closestMrt='Dongmen' />
+          </Col>
+          <Col md="4">
+            <PropertyCard 
+              img='https://urhouse.s3.amazonaws.com/images/rentals/567c4bae0aa0cc886831ee6e8aff6646-watermarked.jpg'
+              title='Test'
+              rent='10000'
+              fullAddress='Some Address'
+              closestMrt='Dongmen' />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/PropertyCard/PropertyCard.js
@@ -0,0 +1,27 @@
+// PropertyCard.js
+import './PropertyCard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import { Card, Button } from 'react-bootstrap';  
+
+function PropertyCard({ img, title, rent, fullAddress, closestMrt}) {
+  return(
+    <Card>  
+      <Card.Img variant="top" src={img} />  
+      <Card.Body>  
+        <Card.Title>{title}</Card.Title>  
+        <Card.Text>  
+          {rent}/month
+        </Card.Text>
+        <Card.Text>  
+          {fullAddress}
+        </Card.Text> 
+        <Card.Text>  
+          {closestMrt}
+        </Card.Text>  
+        <Button variant="primary">Read More</Button>  
+      </Card.Body>  
+    </Card>  
+  )
+}
+
+export default PropertyCard;
